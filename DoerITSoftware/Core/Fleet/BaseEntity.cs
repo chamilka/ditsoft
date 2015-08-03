9886298c6ec3bc109437a519acebcb1da2cf5c4b
chamilka <chamilka@doerit.com>
2015-08-03 15:44:22 +0530

    Fleet management system interface for vehicle enetity and service

---
+++ DoerITSoftware/Core/Fleet/BaseEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DoerITSoftware.Core.Fleet
+{
+    interface BaseEntity
+    {
+        protected void setId(string id);
+        protected void setStatus(Nullable<sbyte> status);
+        protected void setInsertUser(string insertUser);
+        protected void setInsertDatetime(Nullable<System.DateTime> insertDatetime);
+        protected void setUpdateUser(string updateUser);
+        protected void setUpdateDateTime(Nullable<System.DateTime> updateDatetime);
+        protected string getId();
+        protected Nullable<sbyte> getStatus;
+        protected string getInsertUser();
+        protected Nullable<System.DateTime> getInsertDatetime();
+        protected string getUpdateUser();
+        protected Nullable<System.DateTime> getUpdateDateTime();
+        protected string getDescription();
+    }
+}
---
+++ DoerITSoftware/Core/Fleet/Vehicle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoerITSoftware.Core.Fleet
+{
+    interface Vehicle:BaseEntity
+    {
+        protected void setRegistrationNumber(string registrationNumber);
+        protected void setCategory(string category);
+        protected void setType(string type);
+        protected void setMake(string make);
+        protected void setModel(string model);
+        protected void setFuelType(string fuelType);
+        protected void setEngineNumber(string engineNumber);
+        protected void setChassisNumber(string chassisNumber);
+        protected void setYearManufacture(string yearManufacture);
+        protected void setDateImport(string dateImport);
+        protected void setDateRegistered(string dateRegistered);
+        protected void setRegistrationCertificate(byte[] registrationCertificate);
+        protected void setNumWheels(Nullable<sbyte> numWheels);
+        protected void setDescription(string description);
+        protected string getRegistrationNumber();
+        protected string getCategory();
+        protected string getType();
+        protected string getMake();
+        protected string getModel();
+        protected string getFuelType();
+        protected string getEngineNumber();
+        protected string getChassisNumber();
+        protected string getYearManufacture();
+        protected string getDateImport();
+        protected string getDateRegistered();
+        protected byte[] getRegistrationCertificate();
+        protected Nullable<sbyte> getNumWheels();
+        protected string getDescription(string description);
+    }
+}
